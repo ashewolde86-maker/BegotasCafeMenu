fe06c61874727571502d6799f74b7baee89a3a2e
ashewolde86-maker <ashewolde86@gmail.com>
2026-04-30 14:41:27 +0300

    Fix Prettier line endings with endOfLine:lf config

--- admin.js
+++ admin.js
@@ -332,7 +332,10 @@ function initDatabase() {
         localStorage.setItem(DB_KEYS.REVIEWS, JSON.stringify([]));
     }
     if (!localStorage.getItem(DB_KEYS.SETTINGS)) {
-        localStorage.setItem(DB_KEYS.SETTINGS, JSON.stringify({ menuUrl: 'https://begotascafe.netlify.app/menu.html' }));
+        localStorage.setItem(
+            DB_KEYS.SETTINGS,
+            JSON.stringify({ menuUrl: 'https://begotascafe.netlify.app/menu.html' }),
+        );
     }
     if (!localStorage.getItem(DB_KEYS.SUBCATEGORIES) || localStorage.getItem(DB_KEYS.SUBCATEGORIES) === '[]') {
         localStorage.setItem(DB_KEYS.SUBCATEGORIES, JSON.stringify(defaultSubCategories));
